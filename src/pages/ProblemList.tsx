fde6bc9f7615fb3dcb2a05d668e4f999ce1ae680
Xianming Zhong <chinesedfan@qq.com>
2024-03-07 21:24:15 +0800

    fix key warnings and show rating percent

--- src/pages/ProblemList.tsx
+++ src/pages/ProblemList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Tooltip } from 'antd';
 import Problem from '../types/Problem';
-import { getRatingColor } from '../utils';
+import { getRatingColor, getRatingPercent } from '../utils';
 import '../styles/problem.scss';
 
 const { Column } = Table;
@@ -9,9 +9,13 @@ const { Column } = Table;
 interface Props {
     problems: Problem[];
 }
+interface RowData {
+    contest: string;
+    [key: string]: string | Problem;
+}
 export const ProblemList: React.FC<Props> = (props) => {
     const { problems } = props;
-    const contestMap: {[key: string]: {[key: string]: Problem}} = {};
+    const contestMap: {[key: string]: RowData} = {};
     problems.forEach(p => {
         const contest = p.url.split('/')[4];
         contestMap[contest] = contestMap[contest] || {
@@ -22,17 +26,27 @@ export const ProblemList: React.FC<Props> = (props) => {
     const data = Object.keys(contestMap)
         .map(contest => contestMap[contest]);
 
-    return <Table dataSource={data}>
+    return <Table dataSource={data} rowKey={(rowData) => rowData.contest}>
         <Column title="Contest" dataIndex="contest" />
         {
             [1, 2, 3, 4].map(x => (
-                <Column title={"Q" + x} dataIndex={"Q" + x} render={(problem?: Problem) => {
+                <Column title={"Q" + x} dataIndex={"Q" + x} key={x} render={(problem?: Problem) => {
                     if (!problem) return null;
 
                     const color = getRatingColor(problem.rating);
+                    const percent = getRatingPercent(problem.rating);
+                    const circleStyle = {
+                        borderColor: color,
+                        background: `linear-gradient(to top, ${color} ${percent}%, white ${percent}%)`,
+                    };
+                    const spanStyle = {
+                        color,
+                    };
                     return <>
-                        <span className={"difficult-circle " + color}></span>
-                        <span className={"problem-cell " + color}>{problem.name}</span>
+                        <Tooltip title={"Rating: " + problem.rating}>
+                            <span className="difficult-circle " style={circleStyle}></span>
+                        </Tooltip>
+                        <span className="problem-cell" style={spanStyle}>{problem.name}</span>
                     </>;
                 }} />
             ))
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,25 @@
+const LC_RATINGS = [0, 1200, 1400, 1600, 1900, 2100, 2400, 3000];
+const LC_COLORS = [
+    'gray', 'green', '#03a89e' /* cyan */, 'blue', '#a0a' /* violet */,
+    '#ff8c00' /* orange */, 'red'
+];
+
 export function getRatingColor(rating: number): string {
-    if (rating < 1200) {
-        return 'gray';
-    } else if (rating < 1400) {
-        return 'green';
-    } else if (rating < 1600) {
-        return 'cyan';
-    } else if (rating < 1900) {
-        return 'blue';
-    } else if (rating < 2100) {
-        return 'violet';
-    } else if (rating < 2400) {
-        return 'orange';
-    } else {
-        return 'red';
+    for (let i = 1; i < LC_RATINGS.length; i++) {
+        if (rating < LC_RATINGS[i]) {
+            return LC_COLORS[i - 1];
+        }
     }
+    return 'black';
+}
+export function getRatingPercent(rating: number): number {
+    let r = 100;
+    for (let i = 1; i < LC_RATINGS.length; i++) {
+        if (rating < LC_RATINGS[i]) {
+            r = (rating - LC_RATINGS[i - 1]) / (LC_RATINGS[i] - LC_RATINGS[i - 1]);
+            r = Math.floor(r * 100);
+            break;
+        }
+    }
+    return r;
 }
