c333189f63fef75f8bc42e38887d41a1476d4b9f
25lahari <lahari.pasunuti@gmail.com>
2024-07-29 09:17:54 +0530

    updated the required changes!

--- src/components/LegendComponent.tsx
+++ src/components/LegendComponent.tsx
@@ -1,28 +1,31 @@
+import React from 'react';
+
 const data = [
     { color: 'red', label: 'Div 1' },
     { color: 'orange', label: 'Div 2' },
     { color: '#40826D', label: 'Div 3' },
     { color: 'grey', label: 'Div 4' },
-  ];
-  
-  const LegendComponent = () => (
-    <div className="legend-container">
-      <table className="legend-table">
-        <tbody>
-          <tr>
-            {data.map((item, index) => (
-              <td key={index} className="legend-cell">
-                <div className="legend-item">
-                  <span className="legend-color-box" style={{ backgroundColor: item.color }}></span>
-                  <span>{item.label}</span>
-                </div>
-              </td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+];
+
+const LegendComponent = () => (
+    <div className="div-heading-container">
+        <table className="div-heading-table">
+            <tbody>
+                <tr>
+                    {data.map((item, index) => (
+                        <td key={index} className="div-cell">
+                            <div className="div-item">
+                                <span className="div-color-box" style={{ backgroundColor: item.color }}></span>
+                                <span>{item.label}</span>
+                            </div>
+                        </td>
+                    ))}
+                </tr>
+            </tbody>
+        </table>
     </div>
-  );
-  
-  export default LegendComponent;
+);
+
+export default React.memo(LegendComponent);
+
   
