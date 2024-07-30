80f9d061ba9cda666b7935b67419112ef357dc57
25lahari <lahari.pasunuti@gmail.com>
2024-07-30 15:46:19 +0530

    Updated the scss file

--- src/components/LegendComponent.tsx
+++ src/components/LegendComponent.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 
-const data = [
-    { color: 'red', label: 'Div 1' },
-    { color: 'orange', label: 'Div 2' },
-    { color: '#40826D', label: 'Div 3' },
-    { color: 'grey', label: 'Div 4' },
-];
-
 const LegendComponent = () => (
     <div className="div-heading-container">
         <table className="div-heading-table">
             <tbody>
                 <tr>
-                    {data.map((item, index) => (
+                    {[1, 2, 3, 4].map((item, index) => (
                         <td key={index} className="div-cell">
                             <div className="div-item">
-                                <span className="div-color-box" style={{ backgroundColor: item.color }}></span>
-                                <span>{item.label}</span>
+                                <span className={"div-color-box-" + item}></span>
+                                <span>Div {item}</span>
                             </div>
                         </td>
                     ))}
